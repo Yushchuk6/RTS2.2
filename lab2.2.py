0c23fcfb6204e1da160ee770885938274d7b3b6e
Yushchuk6 <yushchuk6@gmail.com>
2020-05-31 14:18:22 +0300

    mini fix

--- lab2.2.py
+++ lab2.2.py
@@ -14,11 +14,11 @@ def my_fft(x):
   fR1, fI1, fR2, fI2 = ([0] * N for i in range(4))
   for p in range(N):
     for k in range(int(N / 2)):
-      fR1[p] += x[2 * k] * cos((2 * pi) / (N / 2) * p * k * 2)
-      fR2[p] += x[2 * k + 1] * cos((2 * pi) / (N / 2) * p * (k * 2 + 1))
+      fR1[p] += x[2 * k] * cos((2 * pi) / N * p * k * 2)
+      fR2[p] += x[2 * k + 1] * cos((2 * pi) / N * p * (k * 2 + 1))
 
-      fI1[p] += x[2 * k] * sin((2 * pi) / (N / 2) * p * k * 2)
-      fI2[p] += x[2 * k + 1] * sin((2 * pi) / (N / 2) * p * (k * 2 + 1))
+      fI1[p] += x[2 * k] * sin((2 * pi) / N * p * k * 2)
+      fI2[p] += x[2 * k + 1] * sin((2 * pi) / N * p * (k * 2 + 1))
     yield sqrt(pow(fR1[p] + fR2[p], 2) + pow(fI1[p] + fI2[p], 2))
 
 
